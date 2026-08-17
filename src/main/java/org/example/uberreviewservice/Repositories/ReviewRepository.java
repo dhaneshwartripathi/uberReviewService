99b25023b2889d8f5f9f912a620e010488e03cc5
dhaneshwar <dhaneshwartripathi685@gmail.com>
2026-08-17 23:56:08 +0530

    Updated review interface

--- src/main/java/org/example/uberreviewservice/Repositories/ReviewRepository.java
+++ src/main/java/org/example/uberreviewservice/Repositories/ReviewRepository.java
@@ -2,11 +2,25 @@ package org.example.uberreviewservice.Repositories;
 
 import org.example.uberreviewservice.Models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
  // during runtime all the implementation/methods of the JpaRepository
 // linked to review will automatically get created from my review repository
+
+    Integer countAllByRatingLessThanEqual(Integer givenRating);
+
+    List<Review> findAllByRatingLessThanEqual(Integer givenRating);
+
+    List<Review> findAllByCreatedAtBefore(Date givenDate);
+    @Query("SELECT r FROM Booking b INNER JOIN b.review r WHERE b.id = :bookingId")
+    Review findReviewByBookingId(Long bookingId);
+
+
 
 }
